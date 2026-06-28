65e60a441e3d4c8c008e4b11fde4446e244ecdf8
Akshay Turkar <turkarakshay59@gmail.com>
2026-06-28 22:51:24 +0530

    added inheritance file

---
+++ JSTest/Inheritance.js
@@ -0,0 +1,36 @@
+
+class vrindavan
+{
+    premanandji='radhe-radhe';
+
+    premandir()
+    {
+        console.log('this is very beautiful temple of the vrindavan')
+    }
+    bankebihari()
+    {
+        console.log('krishna exist here')
+    }
+    nidhivan()
+    {
+        console.log('so many beautiful trees here')
+    }
+}
+class barsana extends vrindavan
+{
+nidhivan()
+{
+    console.log('these are very devine trees of the krishna')
+}
+premanandji='jay shree radhe';
+
+balramji()
+{
+    console.log('krishas big brother')
+}
+}
+let ltst=new barsana();
+console.log(ltst.premanandji);
+ltst.nidhivan();
+ltst.balramji();
+
